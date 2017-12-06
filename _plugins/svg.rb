ac1d483c446aa6470667833be8b18b8e1844dd4c
Jim Killingsworth <jkillingsworth@gmail.com>
2017-12-06 08:16:07 -0800

    Add social media icons

---
+++ _plugins/svg.rb
@@ -0,0 +1,17 @@
+require "open-uri"
+
+module Jekyll
+    class SvgTag < Liquid::Tag
+
+        def initialize(name, input, tokens)
+            @src = input.strip
+        end
+
+        def render(context)
+            text = open(@src, "rt") { |f| data = f.read }
+            text.gsub(/\<\?xml.+\?\>|\<\!DOCTYPE.+\>/, "")
+        end
+    end
+end
+
+Liquid::Template.register_tag("svg", Jekyll::SvgTag)
