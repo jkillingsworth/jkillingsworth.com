3abcf520989db1c2af4b5bd767d82c31239fd544
Jim Killingsworth <jkillingsworth@gmail.com>
2017-12-23 00:23:19 -0800

    Generate local copy of latex equations

--- _plugins/latex.rb
+++ _plugins/latex.rb
@@ -4,8 +4,20 @@ module Jekyll
     class LatexBlock < Liquid::Block
 
         def render(context)
+
             latex = URI.encode(super.strip)
-            "<img src=\"https://latex.codecogs.com/svg.latex?#{latex}\" alt=\"Equation\" />"
+            hash = Digest::SHA1.hexdigest(latex)
+            path = "./assets/latex/#{hash}.svg"
+
+            if !File.exist?(path) then
+                print "Generating #{path} ..."
+                url = "https://latex.codecogs.com/svg.latex?#{latex}"
+                data = open(url, "r") { |f| f.read }
+                open(path, "w") { |f| f.write(data) }
+                print "done.\n"
+            end
+
+            "<img src=\"#{path}\" alt=\"Equation #{hash[0...7]}\" />"
         end
     end
 end
