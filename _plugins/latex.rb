004a55b0ba27c4a1bd2fe7aa5c33bcf1ac2ee52c
Jim Killingsworth <jkillingsworth@gmail.com>
2018-01-06 20:59:11 -0800

    Generate latex equations in context specific asset folder

--- _plugins/latex.rb
+++ _plugins/latex.rb
@@ -1,23 +1,31 @@
-require "open-uri"
-
 module Jekyll
+
     class LatexBlock < Liquid::Block
 
         def render(context)
 
             latex = URI.encode(super.strip)
             hash = Digest::SHA1.hexdigest(latex)
-            path = "./assets/latex/#{hash}.svg"
+            site = context.registers[:site]
+            post = context.registers[:page]
+            path = File.join("_assets", post.path.gsub(/(.*)\/(.*)\.md/, '\2'))
+            file = File.join(path, "#{hash}.svg")
+            name = File.basename(file)
 
-            if !File.exist?(path) then
-                print "Generating #{path} ..."
-                url = "https://latex.codecogs.com/svg.latex?#{latex}"
-                data = open(url, "r") { |f| f.read }
-                open(path, "w") { |f| f.write(data) }
-                print "done.\n"
+            if !Dir.exist?(path) then
+                Dir.mkdir(path)
             end
 
-            "<img src=\"#{path}\" alt=\"Equation #{hash[0...7]}\" />"
+            if !File.exist?(file) then
+                print "Generating #{file}..."
+                url = "https://latex.codecogs.com/svg.latex?#{latex}"
+                data = open(url, "r") { |f| f.read }
+                File.write(file, data)
+                site.static_files << AssetFile.new(site, path, post.url, name)
+                print "done\n"
+            end
+
+            "<img src=\".#{post.url}/#{name}\" alt=\"Equation #{hash[0...7]}\" />"
         end
     end
 end
