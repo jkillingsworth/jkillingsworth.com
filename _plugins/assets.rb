64be2303f79cbe98fbec435510006dfd9a8d926c
Jim Killingsworth <jkillingsworth@gmail.com>
2018-01-10 21:17:42 -0800

    Show gray background when loading equation images

--- _plugins/assets.rb
+++ _plugins/assets.rb
@@ -7,6 +7,26 @@ module Jekyll
         def path
             File.join(@base, @name)
         end
+
+        def write(dest)
+            if path =~ /^.*\/fig-\d{2}-latex-[0-9a-f]{8}\.svg$/ then
+                dest_path = destination(dest)
+
+                return false if File.exist?(dest_path) && !modified?
+                self.class.mtimes[path] = mtime
+
+                FileUtils.mkdir_p(File.dirname(dest_path))
+                FileUtils.rm(dest_path) if File.exist?(dest_path)
+
+                xml = open(path, "r") { |f| Nokogiri::XML(f) }
+                xml.xpath("/xmlns:svg")[0]["style"] = "background: white;"
+                File.write(dest_path, xml)
+
+                true
+            else
+                super
+            end
+        end
     end
 
     class Assets < Generator
