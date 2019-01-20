86dbc6330247b2d5e3f6155c13ca2248946a4659
Jim Killingsworth <jkillingsworth@gmail.com>
2019-01-19 17:27:33 -0800

    Create plugin for encapsulating markdown tables

---
+++ _plugins/table.rb
@@ -0,0 +1,26 @@
+require "open-uri"
+
+module Jekyll
+
+    class TableBlock < Liquid::Block
+
+        def initialize(name, input, tokens)
+            super
+            @input = input.strip
+        end
+
+        def render(context)
+            site = context.registers[:site]
+            conv = site.find_converter_instance(Jekyll::Converters::Markdown)
+            text = super.strip
+
+            opening = "<figure class=\"fig-table\">"
+            content = conv.convert(text)
+            closing = "</figure>"
+
+            opening + content + closing
+        end
+    end
+end
+
+Liquid::Template.register_tag("table", Jekyll::TableBlock)
