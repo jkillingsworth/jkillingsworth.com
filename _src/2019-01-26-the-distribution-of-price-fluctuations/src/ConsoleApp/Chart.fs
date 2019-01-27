9b46da59cf186148ee188a5a6107a27f84471800
Jim Killingsworth <jkillingsworth@gmail.com>
2019-01-26 23:30:02 -0800

    Add source code associated with latest post

---
+++ _src/2019-01-26-the-distribution-of-price-fluctuations/src/ConsoleApp/Chart.fs
@@ -0,0 +1,177 @@
+﻿module Chart
+
+open System
+open System.Diagnostics
+open System.IO
+
+//-------------------------------------------------------------------------------------------------
+
+let private plotPrice = "
+set terminal svg size 720 405 background 'white' font 'Consolas, Monaco, monospace'
+set encoding utf8
+set output '{0}'
+
+$data << EOD
+{1}
+EOD
+
+set xlabel 'Time (Days)'
+set xtics scale 0.01, 0.01
+
+set ylabel 'Market Price (Log Scale)'
+set ytics scale 0.01, 0.01
+set format y '% 1.2f'
+
+set grid xtics ytics mxtics mytics
+set grid linestyle 1 linecolor '#e6e6e6'
+
+set key box linecolor '#808080' samplen 1 opaque
+set key top left reverse Left
+
+set linetype 1 linecolor '#808080'
+
+plot '$data' using 1:2 with lines title '{2}'
+"
+
+//-------------------------------------------------------------------------------------------------
+
+let private plotDiffs = "
+set terminal svg size 720 405 background 'white' font 'Consolas, Monaco, monospace'
+set encoding utf8
+set output '{0}'
+
+$data << EOD
+{1}
+EOD
+
+set xlabel 'Time (Days)'
+set xtics scale 0.01, 0.01
+
+set ylabel 'Price Differences (Log Scale)'
+set ytics scale 0.01, 0.01
+set format y '% 1.2f'
+
+set grid xtics ytics mxtics mytics
+set grid linestyle 1 linecolor '#e6e6e6'
+
+set key box linecolor '#808080' samplen 1 opaque
+set key top left reverse Left
+
+set linetype 1 linecolor '#808080'
+
+plot '$data' using 1:2 with impulses title '{2}'
+"
+
+//-------------------------------------------------------------------------------------------------
+
+let private plotProbs = "
+set terminal svg size 720 405 background 'white' font 'Consolas, Monaco, monospace'
+set encoding utf8
+set output '{0}'
+
+$data << EOD
+{1}
+EOD
+
+set xlabel 'Price Differences (Log Scale), {{/symbol s}} = {6:e3}'
+set xtics scale 0.01, 0.01
+set xrange [-{3}:+{3}]
+set xtics ({4})
+
+set ylabel 'Density'
+set ytics scale 0.01, 0.01
+set format y '%5.0f'
+
+set grid xtics ytics mxtics mytics
+set grid linestyle 1 linecolor '#e6e6e6'
+
+set key box linecolor '#808080' samplen 1 opaque
+set key top left reverse Left
+set key title '{2}' left width 6
+
+set linetype 1 linewidth 1 linecolor '#c0c0c0'
+set linetype 2 linewidth 2 linecolor '#400000ff'
+set linetype 3 linewidth 2 linecolor '#40ff0000'
+
+set style fill solid border linecolor '#808080'
+
+set samples 1000
+distributionN(x,µ,σ) = (1 / (σ * ((2 * pi) ** 0.5))) * exp(-0.5 * ((x - µ) / σ) ** 2)
+distributionL(x,µ,b) = (1 / (2 * b)) * exp(-abs(x - µ) / b)
+
+plot '$data' using 1:2 with boxes title 'Histogram',\
+     distributionN(x, {5}, {6}) title 'Normal',\
+     distributionL(x, {7}, {8}) title 'Laplace'
+"
+
+//-------------------------------------------------------------------------------------------------
+
+let private render plot path (args : obj[]) =
+
+    let file = Path.GetFullPath(path)
+    use proc = new Process()
+    proc.StartInfo.FileName <- "gnuplot.exe"
+    proc.StartInfo.UseShellExecute <- false
+    proc.StartInfo.RedirectStandardInput <- true
+    proc.Start() |> ignore
+    proc.StandardInput.Write(plot, args |> Array.append [| file |])
+    proc.StandardInput.Flush()
+
+let private makeLabel (descriptor : string) =
+    let items = descriptor.Split("-")
+    let period (item : string) = Char.ToUpper(item.[0]).ToString() + item.Substring(1)
+    let symbol (item : string) = if (item.Length = 6) then item.Insert(3, "/") else item
+    sprintf "%-15s" <| sprintf "%s (%s)" (symbol items.[2]) (period items.[1])
+
+//-------------------------------------------------------------------------------------------------
+
+let renderPrice path data =
+
+    let descriptor, data = data
+    let label = makeLabel descriptor
+
+    let data =
+        data
+        |> Array.mapi (fun i x -> sprintf "%i %f" i x)
+        |> String.concat "\n"
+
+    render plotPrice path [| data; label |]
+
+//-------------------------------------------------------------------------------------------------
+
+let renderDiffs path data =
+
+    let descriptor, data = data
+    let label = makeLabel descriptor
+
+    let data =
+        data
+        |> Array.mapi (fun i x -> sprintf "%i %f" i x)
+        |> String.concat "\n"
+
+    render plotDiffs path [| data; label |]
+
+//-------------------------------------------------------------------------------------------------
+
+let renderProbs path data =
+
+    let descriptor, histogram, sigmas, (µN, σN), (µL, bL) = data
+    let label = makeLabel descriptor
+    let xwide = sigmas * σN : float
+    let n = int sigmas
+
+    let xlabel = function
+        | 0 -> "0"
+        | i -> sprintf "'%+i{{/symbol s}}' %f" i (float i * σN)
+
+    let xrange =
+        [| -n .. +n |]
+        |> Array.map xlabel
+        |> Array.reduce (fun l r -> l + ", " + r)
+
+    let data =
+        histogram
+        |> Array.map (fun (center, amount) -> sprintf "%f %f" center amount)
+        |> String.concat "\n"
+
+    render plotProbs path [| data; label; xwide; xrange; µN; σN; µL; bL |]
---
+++ _src/2019-01-26-the-distribution-of-price-fluctuations/src/ConsoleApp/Program.fs
@@ -0,0 +1,137 @@
+﻿module Program
+
+open System
+
+//-------------------------------------------------------------------------------------------------
+
+let sigmas = 4.0
+
+let path filename = "../../../" + filename
+
+//-------------------------------------------------------------------------------------------------
+
+type OutputFlag =
+    | Price
+    | Diffs
+    | Probs
+    | Stats
+
+let output flag (dataset : Data.Dataset) =
+    let descriptor = dataset.Descriptor
+    let prices : float[] = dataset.GetPrices()
+    let logprices = prices |> Array.map log
+    let differences = logprices |> Compute.differences
+    let criteriaN = differences |> Compute.fitDistributionN
+    let criteriaL = differences |> Compute.fitDistributionL
+    let wide = criteriaN |> snd |> (*) sigmas
+    let histogram = differences |> Compute.histogram wide
+    let dataPrice = (descriptor, logprices)
+    let dataDiffs = (descriptor, differences)
+    let dataProbs = (descriptor, histogram, sigmas, criteriaN, criteriaL)
+    match flag with
+    | Price -> dataPrice |> Chart.renderPrice (path descriptor + "-price.svg")
+    | Diffs -> dataDiffs |> Chart.renderDiffs (path descriptor + "-diffs.svg")
+    | Probs -> dataProbs |> Chart.renderProbs (path descriptor + "-probs.svg")
+    | Stats ->
+        printfn "----------------------------------------"
+        printfn "%s" dataset.Descriptor
+        printfn "µN = %+.10f" (fst criteriaN)
+        printfn "σN = %+.10f" (snd criteriaN)
+        printfn "µL = %+.10f" (fst criteriaL)
+        printfn "bL = %+.10f" (snd criteriaL)
+        printfn ""
+
+//-------------------------------------------------------------------------------------------------
+
+output Price <| Data.StocksDaily("SPY")
+output Diffs <| Data.StocksDaily("SPY")
+output Probs <| Data.StocksDaily("SPY")
+output Stats <| Data.StocksDaily("SPY")
+
+//-------------------------------------------------------------------------------------------------
+
+output Probs <| Data.StocksDaily("DIA")
+output Probs <| Data.StocksDaily("EEM")
+output Probs <| Data.StocksDaily("GLD")
+output Probs <| Data.StocksDaily("HYG")
+output Probs <| Data.StocksDaily("LQD")
+output Probs <| Data.StocksDaily("TLT")
+output Probs <| Data.StocksDaily("UNG")
+output Probs <| Data.StocksDaily("USO")
+
+//-------------------------------------------------------------------------------------------------
+
+output Probs <| Data.StocksDaily("AMZN")
+output Probs <| Data.StocksDaily("AZO")
+output Probs <| Data.StocksDaily("BLK")
+output Probs <| Data.StocksDaily("CAT")
+output Probs <| Data.StocksDaily("CMG")
+output Probs <| Data.StocksDaily("FDX")
+output Probs <| Data.StocksDaily("GM")
+output Probs <| Data.StocksDaily("GOOG")
+output Probs <| Data.StocksDaily("GWW")
+output Probs <| Data.StocksDaily("HUM")
+output Probs <| Data.StocksDaily("NFLX")
+output Probs <| Data.StocksDaily("TSLA")
+output Probs <| Data.StocksDaily("TWLO")
+output Probs <| Data.StocksDaily("ULTA")
+output Probs <| Data.StocksDaily("UNH")
+
+//-------------------------------------------------------------------------------------------------
+
+output Probs <| Data.StocksIntraday("AMZN")
+output Probs <| Data.StocksIntraday("AZO")
+output Probs <| Data.StocksIntraday("BLK")
+output Probs <| Data.StocksIntraday("CAT")
+output Probs <| Data.StocksIntraday("CMG")
+output Probs <| Data.StocksIntraday("FDX")
+output Probs <| Data.StocksIntraday("GM")
+output Probs <| Data.StocksIntraday("GOOG")
+output Probs <| Data.StocksIntraday("GWW")
+output Probs <| Data.StocksIntraday("HUM")
+output Probs <| Data.StocksIntraday("NFLX")
+output Probs <| Data.StocksIntraday("TSLA")
+output Probs <| Data.StocksIntraday("TWLO")
+output Probs <| Data.StocksIntraday("ULTA")
+output Probs <| Data.StocksIntraday("UNH")
+
+//-------------------------------------------------------------------------------------------------
+
+output Price <| Data.StocksDaily("VIX")
+output Diffs <| Data.StocksDaily("VIX")
+output Probs <| Data.StocksDaily("VIX")
+output Probs <| Data.StocksIntraday("VIX")
+
+//-------------------------------------------------------------------------------------------------
+
+output Price <| Data.ForexDaily("EURUSD")
+output Diffs <| Data.ForexDaily("EURUSD")
+output Probs <| Data.ForexDaily("EURUSD")
+
+//-------------------------------------------------------------------------------------------------
+
+output Probs <| Data.ForexDaily("USDJPY")
+output Probs <| Data.ForexDaily("USDMXN")
+output Probs <| Data.ForexDaily("USDRUB")
+output Probs <| Data.ForexDaily("USDTRY")
+output Probs <| Data.ForexDaily("USDZAR")
+output Probs <| Data.ForexDaily("EURNOK")
+output Probs <| Data.ForexDaily("EURSEK")
+output Probs <| Data.ForexDaily("EURTRY")
+
+//-------------------------------------------------------------------------------------------------
+
+output Probs <| Data.ForexIntraday("USDJPY")
+output Probs <| Data.ForexIntraday("USDMXN")
+output Probs <| Data.ForexIntraday("USDRUB")
+output Probs <| Data.ForexIntraday("USDTRY")
+output Probs <| Data.ForexIntraday("USDZAR")
+output Probs <| Data.ForexIntraday("EURNOK")
+output Probs <| Data.ForexIntraday("EURSEK")
+output Probs <| Data.ForexIntraday("EURTRY")
+
+//-------------------------------------------------------------------------------------------------
+
+output Probs <| Data.CryptoDaily("BTC")
+output Probs <| Data.CryptoDaily("ETH")
+output Probs <| Data.CryptoDaily("XRP")
