05efa01ebcb9471ee5e7a71b2954735a22dd97e9
Jim Killingsworth <jkillingsworth@gmail.com>
2019-02-07 19:11:12 -0800

    Change stats number format

--- _src/2019-01-26-the-distribution-of-price-fluctuations/src/ConsoleApp/Program.fs
+++ _src/2019-01-26-the-distribution-of-price-fluctuations/src/ConsoleApp/Program.fs
@@ -35,10 +35,10 @@ let output flag (dataset : Data.Dataset) =
     | Stats ->
         printfn "----------------------------------------"
         printfn "%s" dataset.Descriptor
-        printfn "µN = %+.10f" (fst criteriaN)
-        printfn "σN = %+.10f" (snd criteriaN)
-        printfn "µL = %+.10f" (fst criteriaL)
-        printfn "bL = %+.10f" (snd criteriaL)
+        printfn "µN = %+.3e" (fst criteriaN)
+        printfn "σN = %+.3e" (snd criteriaN)
+        printfn "µL = %+.3e" (fst criteriaL)
+        printfn "bL = %+.3e" (snd criteriaL)
         printfn ""
 
 //-------------------------------------------------------------------------------------------------
