bde176ad32a8099e3241049332547725f68de963
Jim Killingsworth <jkillingsworth@gmail.com>
2019-02-13 15:46:05 -0800

    Consolidate hyphenation fixups into separate plugin

---
+++ _plugins/hyphenation.rb
@@ -0,0 +1,16 @@
+require "jekyll/hyphenate_filter"
+
+module Jekyll
+
+    module HyphenationFilter
+
+        def hyphenate_and_fixup(content)
+            content = hyphenate(content)
+            content = content.gsub("-\u00ad", "-")
+            content = content.gsub("&amp;am\u00adp;", "&amp;")
+            content
+        end
+    end
+end
+
+Liquid::Template.register_filter(Jekyll::HyphenationFilter)
