c246153b8a4ec32ede16edaa80b1df82a672c01d
Jim Killingsworth <jkillingsworth@gmail.com>
2020-02-06 10:03:42 -0800

    Create improved version of the latextosvg script

--- tools/latextosvg.sh
+++ tools/latextosvg.sh
@@ -2,25 +2,88 @@
 
 set -e
 
-if [ -z "${1}" ]; then
-    echo "Usage: ${0} TEXFILE [SVGFILE]"
+show_help() {
+    echo "Usage: $(basename ${0}) [OPTION]... [TEXFILE [SVGFILE]]"
+}
+
+option_fonts=nofonts
+
+options=$(getopt -n "${0}" -o f: -l fonts:,help -- "${@}")
+
+eval set -- "${options}"
+
+while true; do case "${1}" in
+    -f | --fonts )
+        option_fonts="${2}"
+        shift 2
+        ;;
+    --help )
+        show_help
+        exit 0
+        ;;
+    -- )
+        shift 1
+        break
+        ;;
+    * )
+        echo -e "$(basename ${0}): unhandled case -- ${1}\a" 1>&2
+        exit 1
+        ;;
+esac done
+
+if [ -t -0 ] && [ -z "${1}" ]; then
+    show_help
     exit 1
 fi
 
+texfile_i=${1:-/dev/stdin}
+svgfile_o=${2:-/dev/stdout}
 tempdir=$(mktemp -d)
-texfile=$(realpath "${1}")
-svgfile=output.svg
-jobname=latextosvg
+jobname="latextosvg"
 
-if [ -n "${2}" ]; then svgfile="${2}"; fi
+convert_tex_to_dvi() {
 
-pushd ${tempdir} > /dev/null
+    set +e
+    clargs="--halt-on-error --interaction=nonstopmode"
+    output=$(latex ${clargs} --output-dir="${tempdir}" "${jobname}.tex")
+    result=${?}
+    set -e
 
-cp "${texfile}" ${jobname}.tex
-latex --halt-on-error --interaction=nonstopmode ${jobname}.tex
-dvisvgm ${jobname}.dvi --no-fonts --exact --zoom=1.333333 --precision=6 --verbosity=7
+    if [ ${result} != 0 ]; then
+        echo "${output}" 1>&2
+        exit 1
+    fi
+}
+
+convert_dvi_to_svg() {
+
+    clargs="${1} --exact --zoom=1.333333 --precision=6 --verbosity=3"
+    output=$(dvisvgm ${clargs} "${jobname}.dvi" 2>&1)
+
+    if [ -n "${output}" ]; then
+        echo "${output}" 1>&2
+        exit 1
+    fi
+}
+
+cat "${texfile_i}" > "${tempdir}/${jobname}.tex"
+pushd "${tempdir}" > /dev/null
+
+case "${option_fonts}" in
+    nofonts )
+        convert_tex_to_dvi
+        convert_dvi_to_svg "--no-fonts"
+        ;;
+    ttfonts )
+        convert_tex_to_dvi
+        convert_dvi_to_svg "--font-format=ttf"
+        ;;
+    * )
+        echo -e "$(basename ${0}): invalid font option -- ${option_fonts}\a" 1>&2
+        exit 1
+        ;;
+esac
 
 popd > /dev/null
-
-cp ${tempdir}/${jobname}.svg "${svgfile}"
-rm ${tempdir} -rf
+cp "${tempdir}/${jobname}.svg" "${svgfile_o}"
+rm "${tempdir}" -rf
