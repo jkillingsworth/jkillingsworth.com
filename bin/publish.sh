c73f4623d73d2af494d83c39944ae07c16a2ae45
Jim Killingsworth <jkillingsworth@gmail.com>
2020-03-12 20:14:32 -0700

    Update publish script to create new local repo each time

--- bin/publish.sh
+++ bin/publish.sh
@@ -8,11 +8,19 @@ basedir=$(dirname "${0}" | xargs -I% realpath %/..)
 
 cd "${basedir}"
 
+rm -rf _site/*
+rm -rf _site/.git
+
 JEKYLL_ENV=production ./bin/jekyll build
 
 cd _site
 
-git update-ref -d HEAD
+github_repo=https://github.com/jkillingsworth/jkillingsworth.com.git
+
+git init
+git remote add origin ${github_repo}
+git config core.safecrlf false
+git checkout -b gh-pages
 git add -A
 git commit -m "Publish"
 git push origin gh-pages -f
