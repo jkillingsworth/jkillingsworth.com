af8d09f0f8fe0e567e0dae16039de8c9170195e1
Jim Killingsworth <jkillingsworth@gmail.com>
2020-04-03 12:24:50 -0700

    Remove background color processing step for svg images

--- _plugins/assets.rb
+++ _plugins/assets.rb
@@ -6,26 +6,6 @@ module Jekyll
         def path
             File.join(@base, @name)
         end
-
-        def write(dest)
-            if path =~ /^.*\/fig-\d{2}-.+\.svg$/ then
-                dest_path = destination(dest)
-
-                return false if File.exist?(dest_path) && !modified?
-                self.class.mtimes[path] = mtime
-
-                FileUtils.mkdir_p(File.dirname(dest_path))
-                FileUtils.rm(dest_path) if File.exist?(dest_path)
-
-                xml = open(path, "r") { |f| Nokogiri::XML(f) }
-                xml.xpath("/xmlns:svg")[0]["style"] = "background: white;"
-                File.write(dest_path, xml)
-
-                true
-            else
-                super
-            end
-        end
     end
 
     class Assets < Generator
