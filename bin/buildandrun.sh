a8c09fcd7dda7f07e29f8768ea1aea41d37af112
Jim Killingsworth <jkillingsworth@gmail.com>
2020-09-14 04:16:31 -0700

    Create script to build and run source code associated with blog posts

---
+++ bin/buildandrun.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+#--------------------------------------------------------------------------------------------------
+
+basedir=$(dirname "${0}" | xargs -I% realpath %/..)
+
+cd "${basedir}"
+
+#--------------------------------------------------------------------------------------------------
+
+run_console_app()
+{
+    pushd "${dir}"/build/bin/Release/netcoreapp3.1
+    ./ConsoleApp
+    popd
+}
+
+for dir in ./src/20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]-*; do
+    rm -rf "${dir}"/build
+    dotnet build -c Release "${dir}"/Workspace.sln
+    run_console_app > /dev/null
+done
