83b9ceea1362b97d07570ded091ce90c4abc37c2
Jim Killingsworth <jkillingsworth@gmail.com>
2021-07-18 03:06:35 -0700

    Break dependence on third party jekyll plugin for hyphenation

--- Gemfile
+++ Gemfile
@@ -1,10 +1,8 @@
 source "https://rubygems.org"
 
-group :jekyll_plugins do
-    gem "jekyll-hyphenate_filter"
-end
-
 gem "htmlcompressor"
 gem "jekyll"
+gem "nokogiri"
+gem "text-hyphen"
 gem 'wdm', '~> 0.1.1', :install_if => Gem.win_platform?
 gem "webrick"
--- _plugins/hyphenation.rb
+++ _plugins/hyphenation.rb
@@ -1,14 +1,37 @@
-require "jekyll/hyphenate_filter"
+require "nokogiri"
+require "text/hyphen"
 
 module Jekyll
 
     module HyphenationFilter
+
+        @@hyphenator = Text::Hyphen.new(language: "en_us", :left => 2, :right => 2)
 
         def hyphenate_and_fixup(content)
             content = hyphenate(content)
             content = content.gsub(/-\u00ad/, "-")
             content = content.gsub(/&amp;am(\u00ad?)p;/, "&amp;")
             content
+        end
+
+        def hyphenate(content)
+            selector = @context.registers[:site].config["hyphenation"]["selector"]
+            fragment = Nokogiri::HTML::DocumentFragment.parse(content)
+            fragment.css(selector).each do |element|
+                element.traverse do |node|
+                    node.content = hyphenate_text(node.to_s) if node.text?
+                end
+            end
+            fragment.to_s
+        end
+
+        def hyphenate_text(text)
+            text.split.each do |word|
+                regex = /#{Regexp.escape(word)}(?!\z)/
+                hyphenated_word = @@hyphenator.visualize(word, "\u00ad")
+                text.gsub!(regex, hyphenated_word)
+            end
+            text
         end
     end
 end
