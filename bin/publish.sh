bdbf789c24bd4e2101eaf4511a99f0cb3b4b7eed
Jim Killingsworth <jkillingsworth@gmail.com>
2022-01-25 17:54:33 -0800

    Update publish script with better deployment workflow

--- bin/publish.sh
+++ bin/publish.sh
@@ -11,16 +11,13 @@ cd "${basedir}"
 rm -rf _site/*
 rm -rf _site/.git
 
+git clone https://github.com/jkillingsworth/jkillingsworth.com.git _site -b gh-pages --depth 1
+
 JEKYLL_ENV=production ./bin/jekyll build
 
 cd _site
 
-github_repo=https://github.com/jkillingsworth/jkillingsworth.com.git
-
-git init
-git remote add origin ${github_repo}
 git config core.safecrlf false
-git checkout -b gh-pages
 git add -A
 git commit -m "Publish"
-git push origin gh-pages -f
+git push origin gh-pages
