9558646abd7d99b7164e95ddab0a13b94cd09f5b
Jim Killingsworth <jkillingsworth@gmail.com>
2023-05-31 18:27:35 -0700

    Remove build props and use default build output paths

--- bin/buildandrun.sh
+++ bin/buildandrun.sh
@@ -12,16 +12,19 @@ cd "${basedir}"
 
 run_console_app()
 {
-    pushd "${dir}"/build/bin-ConsoleApp/Release/net7.0
+    pushd "${dir}"/src/ConsoleApp/bin/Release/net7.0
     ./ConsoleApp
     popd
 }
 
-rm -rf ./src/Common/build
+rm -rf ./src/Common/src/Common/bin
+rm -rf ./src/Common/src/Common/obj
 dotnet build -c Release ./src/Common/Workspace.sln
 
 for dir in ./src/20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]-*; do
-    rm -rf "${dir}"/build
+    rm -rf "${dir}"/output
+    rm -rf "${dir}"/src/ConsoleApp/bin
+    rm -rf "${dir}"/src/ConsoleApp/obj
     dotnet build -c Release "${dir}"/Workspace.sln
     run_console_app > /dev/null
 done
